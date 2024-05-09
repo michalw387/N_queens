13423839cb83a100e040924a00b72227d5e959af
michalw387 <michal.wisniewski387@gmail.com>
2024-05-09 18:17:52 +0200

    first commit

---
+++ 8_queens.py
@@ -0,0 +1,70 @@
+from docplex.cp.model import CpoModel
+import json
+
+# -----------------------------------------------------------------------------
+# Initialize the problem data
+# -----------------------------------------------------------------------------
+
+# Set model parameters
+NB_QUEEN = 8
+
+
+# -----------------------------------------------------------------------------
+# Build the model
+# -----------------------------------------------------------------------------
+
+# Create model
+mdl = CpoModel()
+
+# Create column index of each queen
+locations = mdl.integer_var_list(NB_QUEEN, 0, NB_QUEEN - 1, "X")
+
+# One queen per row
+mdl.add(mdl.all_diff(locations))
+
+# One queen per diagonal xi - xj != i - j
+mdl.add(mdl.all_diff(locations[i] + i for i in range(NB_QUEEN)))
+
+# One queen per diagonal xi - xj != j - i
+mdl.add(mdl.all_diff(locations[i] - i for i in range(NB_QUEEN)))
+
+
+# -----------------------------------------------------------------------------
+# Solve the model and display the result
+# -----------------------------------------------------------------------------
+
+# Solve model
+print("Solving model....")
+msol = mdl.solve(TimeLimit=10)
+
+# Print solution
+if msol:
+    print("Solution:", end="")
+    for v in locations:
+        print(f" {msol[v]}", end="")
+    print("\n")
+    # Draw chess board
+    for l in range(NB_QUEEN):
+        qx = msol[locations[l]]
+        for c in range(NB_QUEEN):
+            field = "1" if c == qx else "0"
+            print(field + " ", end="")
+        print("")
+    # Save solutions to a file
+
+    # Create a list to store the chess board
+    chess_board = []
+
+    # Populate the chess board
+    for l in range(NB_QUEEN):
+        qx = msol[locations[l]]
+        row = []
+        for c in range(NB_QUEEN):
+            row.append(1 if c == qx else 0)
+        chess_board.append(row)
+
+    # Save the chess board as JSON
+    with open(f"{NB_QUEEN}_queens.json", "w") as file:
+        json.dump(chess_board, file)
+else:
+    print("Solve status: {}\n".format(msol.get_solve_status()))
---
+++ show_3d.py
@@ -0,0 +1,239 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+
+def get_file_path(filename, dim=3):
+    file_path = "locations_files\\"
+
+    if dim == 3:
+        file_path += "3D\\"
+    elif dim == 2:
+        file_path += "2D\\"
+
+    file_path += filename
+    return file_path
+
+
+def import_locations_from_txt(filename="8_towers.txt", dim=3):
+    file_path = get_file_path(filename, dim)
+    with open(file_path, "r") as file:
+        locations = file.read()
+    return convert_text_to_list(locations)
+
+
+def import_locations_from_json(filename="8_towers.json", dim=3):
+    file_path = get_file_path(filename, dim)
+    with open(file_path, "r") as file:
+        raw_locations = file.read()
+    return np.array(json.loads(raw_locations)).ravel()
+
+
+def convert_text_to_list(raw_text):
+    # Remove unnecessary characters and split by lines
+    lines = raw_text.replace("[[", "[").replace("]]", "]").split("\n")
+
+    # Remove empty lines and whitespace
+    lines = [line.strip() for line in lines if line.strip()]
+
+    lines = [line.replace(" ", ",") for line in lines]
+
+    # Join the lines and evaluate as Python code
+    nested_list = eval(",".join(lines))
+
+    # Flatten the nested list
+    flat_list = [item for sublist in nested_list for item in sublist]
+
+    return flat_list
+
+
+def color_points_by_location(locations, points):
+    red_points = []
+    blue_points = []
+
+    for point in points:
+        red_points.append(
+            [point[i] for i in range(len(locations)) if locations[i] == 1]
+        )
+        blue_points.append(
+            [point[i] for i in range(len(locations)) if locations[i] == 0]
+        )
+
+    return np.array(red_points), np.array(blue_points)
+
+
+def add_labels(ax, locations, x, y, z=None):
+    for i in range(len(locations)):
+        ax.text(
+            x[i],
+            y[i],
+            z[i],
+            f"{x[i]} {y[i]} {z[i]}",
+            color="black",
+            fontsize=8,
+            ha="center",
+            va="bottom",
+        )
+
+
+def show_in_2d(locations):
+    grid_size = round(len(locations) ** (1 / 2))
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.subplots()
+
+    x = np.array(range(grid_size))
+    y = np.array(range(grid_size))
+    X, Y = np.meshgrid(x, y)
+    x, y = X.ravel(), Y.ravel()
+
+    red_points, blue_points = color_points_by_location(locations, np.array([x, y]))
+
+    ax.scatter(
+        blue_points[0],
+        blue_points[1],
+        c="b",
+        marker="o",
+        s=100,
+        label="Empty position",
+    )
+    ax.scatter(
+        red_points[0],
+        red_points[1],
+        c="r",
+        marker="o",
+        s=100,
+        label="Queen's position",
+    )
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+
+    ax.set_xticks(np.arange(0, grid_size))
+    ax.set_yticks(np.arange(0, grid_size))
+
+    plt.title(f"Grid for {grid_size}x{grid_size}", fontsize=20)
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+def show_in_3d(locations, color_points=True, labels=False, print_connections=False):
+    points_size = 50
+    points_size_ratio = 0.3
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection="3d")
+
+    grid_size = round(len(locations) ** (1 / 3))
+
+    # # Create 3D grid
+    x = np.array(range(grid_size))
+    y = np.array(range(grid_size))
+    z = np.array(range(grid_size))
+    X, Y, Z = np.meshgrid(x, y, z)
+    # Y, Z, X = np.meshgrid(x, y, z)
+    x, y, z = X.ravel(), Y.ravel(), Z.ravel()
+
+    if color_points:
+
+        red_points, blue_points = color_points_by_location(
+            locations, np.array([x, y, z])
+        )
+
+        ax.scatter(
+            blue_points[0],
+            blue_points[1],
+            blue_points[2],
+            c="b",
+            marker="o",
+            s=points_size * pow(grid_size, 1 / 5) * points_size_ratio,
+        )
+        ax.scatter(
+            red_points[0],
+            red_points[1],
+            red_points[2],
+            c="r",
+            marker="o",
+            s=points_size * pow(grid_size, 1 / 5),
+        )
+
+    else:
+        ax.scatter(
+            x, y, z, c=x + y + z, marker="o", s=points_size * pow(grid_size, 1 / 3)
+        )
+
+    if labels:
+        add_labels(ax, locations, x, y, z)
+
+    if print_connections:
+        for i in range(len(locations)):
+            for j in range(i + 1, len(locations)):
+                if (
+                    (x[i] == x[j] and y[i] == y[j])
+                    or (y[i] == y[j] and z[i] == z[j])
+                    or (x[i] == x[j] and z[i] == z[j])
+                ):
+                    ax.plot(
+                        [x[i], x[j]],
+                        [y[i], y[j]],
+                        [z[i], z[j]],
+                        c="k",
+                        linewidth=0.5,
+                    )
+
+    ax.set_xlabel("X Label")
+    ax.set_ylabel("Y Label")
+    ax.set_zlabel("Z Label")
+
+    ax.set_xticks(np.arange(0, grid_size))
+    ax.set_yticks(np.arange(0, grid_size))
+    ax.set_zticks(np.arange(0, grid_size))
+
+    plt.title(f"Grid for {grid_size}x{grid_size}x{grid_size}", fontsize=20)
+    plt.show()
+
+
+def print_indexes(grid_size):
+    for z_offset in range(grid_size):
+        print("main loop", z_offset + 1)
+        for x_offset in range(grid_size - z_offset):
+            print("\tsub loop", x_offset + 1)
+            for i in range(min(grid_size - x_offset, grid_size - z_offset)):
+                print("\t\t", i + x_offset, i, i + z_offset)
+
+
+def generate_empty_locations(grid_size):
+    locations = np.zeros((grid_size, grid_size, grid_size)).flatten()
+    return locations
+
+
+def show_from_files(filenames, dim=3):
+    for filename in filenames:
+        if filename.endswith(".json"):
+            locations = import_locations_from_json(filename, dim)
+        else:
+            locations = import_locations_from_txt(filename, dim)
+        if dim == 3:
+            show_in_3d(locations)
+        elif dim == 2:
+            show_in_2d(locations)
+
+
+filenames_txt = ["3_towers.txt", "8_towers.txt"]
+show_from_files(filenames_txt)
+
+
+empty_locations = generate_empty_locations(4)
+show_in_3d(empty_locations)
+show_in_3d(empty_locations, labels=True)
+
+
+empty_locations = generate_empty_locations(6)
+show_in_3d(empty_locations, color_points=False)
+
+
+print_indexes(4)
+
+
+filenames_json = ["8_queens.json"]
+show_from_files(filenames_json, dim=2)
